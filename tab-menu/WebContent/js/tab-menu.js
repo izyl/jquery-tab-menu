81e2efbca0e7a2289f863a008da521ba92247dbb
izyl <l.schvent@yahoo.fr>
2013-04-27 21:53:58 +0200

    commentaires

--- tab-menu/WebContent/js/tab-menu.js
+++ tab-menu/WebContent/js/tab-menu.js
@@ -2,17 +2,32 @@
 
 	$.fn.tabMenu = function(options) {
 
+		/** le tab courant, calculé à l'initialisation du plugin (on pourrait exposer un setter) */
 		var currentTab;
 
 		// Create some defaults, extending them with any options that were provided
 		var settings = $.extend({
+			/** l'id du container de menu */
 			tabMenuId : 'tab-menu',
+			/** l'id de la ligne principale d'onglets */
 			rootRowId : 'rootRow',
+			/** le nom de la classe css pour l'état actif des tabs */
 			activeClass : 'active',
+			/** le type d'élément pour représenter une ligne d'onglets */
 			rowElement : 'div',
+			/** l'attribut dans lequel on stock le lien entre l'anchor et le div d'onglet fils associé */
 			rowTargetAttr : 'target',
-			activeElement : 'a',
+			/**
+			 * le type d'élément associé à un onglet (on pourrait mettre des images par exemples, mais mieux, on pourrait rendre générique afin de pouvoir
+			 * utiliser différents éléments dans un meme menu ?)
+			 */
+			tabElement : 'a',
+			/** c'est ce timeout qui rend le menu ergonomique en autorisant des "sorties de routes" */
 			timeout : 500,
+			/**
+			 * la structure du menu récupéré en json, s'il n'est pas vide, il sera injecté dans le rootMenuId (qui devra donc être vide avant l'injection). Pour
+			 * vpm, il devra contenir le nom, le href et ses fils.
+			 */
 			jsonMenu : {}
 		}, options);
 
@@ -23,10 +38,10 @@
 			}
 
 			// le a marqué avec la classe active à l'ouverture de la page devient le tab courant
-			currentTab = $("#" + settings.tabMenuId + " " + settings.activeElement + "." + settings.activeClass);
+			currentTab = $("#" + settings.tabMenuId + " " + settings.tabElement + "." + settings.activeClass);
 			var active = currentTab;
 			// on active le tag courant
-			activeToTop(active);
+			activate(active);
 
 			// quand on sort du menu on rétabli l'onglet courant
 			$("#" + settings.tabMenuId).hoverIntent({
@@ -40,11 +55,11 @@
 				timeout : settings.timeout
 			});
 
-			$("#" + settings.tabMenuId + " " + settings.activeElement).hoverIntent({
+			$("#" + settings.tabMenuId + " " + settings.tabElement).hoverIntent({
 				over : function() {
 
 					active = $(this);
-					activeToTop(active);
+					activate(active);
 				},
 				out : function() {
 					$.noop();
@@ -63,22 +78,21 @@
 		}
 
 		// active l'onglet survolé ainsi que tous ses tab parents et cache le reste (à optimiser ?)
-		function activeToTop(active) {
+		function activate(active) {
 			// reset le menu.
 			reset();
+			// on habille l'onglet actif
 			active.addClass(settings.activeClass);
+			// et on montre le div associé
 			$(active.attr(settings.rowTargetAttr)).show();
 
 			// N'ayez pas peur :D, c'est simple en fait
-			// on sélectionne vers le haut tous les div jusqu'au container du menu (:tabMenu)
+			// on sélectionne vers le haut toutes les lignes jusqu'au container du menu (:tabMenu)
 			active.parentsUntil($("#" + settings.tabMenuId), settings.rowElement).each(function() {
-				// pour chaque div on va remonter au parent puis on va choisir le li dont la valeur vaut l'id du div. Sur le a contenu dans ce li on va
-				// mettre
-				// la classe active
-				// on raffiche le div
+				// on affiche chaque ligne
 				$(this).show();
-				// $(this).children(settings.activeElement).show();
-				$(this).parent().find(settings.activeElement + "[" + settings.rowTargetAttr + "=#" + $(this).attr("id") + "]").addClass(settings.activeClass);
+				// et on habille le lien qui pointe dessus
+				$(this).parent().find(settings.tabElement + "[" + settings.rowTargetAttr + "=#" + $(this).attr("id") + "]").addClass(settings.activeClass);
 
 			});
 		}
@@ -86,14 +100,13 @@
 		// permet de restorer le tab initial
 		function restoreTab() {
 
-			// re-set le tab courant.
-			activeToTop(currentTab);
+			activate(currentTab);
 		}
 
 		// cache toutes les lignes et désactive tous les tabs
 		function reset() {
 			hideAll();
-			$("#" + settings.tabMenuId + " " + settings.activeElement).each(function() {
+			$("#" + settings.tabMenuId + " " + settings.tabElement).each(function() {
 				$(this).removeClass(settings.activeClass);
 			});
 		}
