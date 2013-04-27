7a1d36d37c929a4ad2b817c1a909c2962d9cab4c
izyl <l.schvent@yahoo.fr>
2013-04-27 21:37:50 +0200

    refactor structure html et settings note : la selection de a par champs hidden ne fonctionne pas j'ai du utiliser le champs target

--- tab-menu/WebContent/js/tab-menu.js
+++ tab-menu/WebContent/js/tab-menu.js
@@ -10,9 +10,9 @@
 			rootRowId : 'rootRow',
 			activeClass : 'active',
 			rowElement : 'div',
-			rowTargetAttr : 'value',
-			activeElement : 'li',
-			timeout : 300,
+			rowTargetAttr : 'target',
+			activeElement : 'a',
+			timeout : 500,
 			jsonMenu : {}
 		}, options);
 
@@ -22,11 +22,11 @@
 				buildMenu(settings.jsonMenu);
 			}
 
-			$("#" + settings.rootRowId).show();
+			// le a marqué avec la classe active à l'ouverture de la page devient le tab courant
 			currentTab = $("#" + settings.tabMenuId + " " + settings.activeElement + "." + settings.activeClass);
 			var active = currentTab;
-
-			activeToTop($(active.attr(settings.rowTargetAttr)));
+			// on active le tag courant
+			activeToTop(active);
 
 			// quand on sort du menu on rétabli l'onglet courant
 			$("#" + settings.tabMenuId).hoverIntent({
@@ -43,15 +43,7 @@
 			$("#" + settings.tabMenuId + " " + settings.activeElement).hoverIntent({
 				over : function() {
 
-					// reset la class active
-					resetClass(settings.activeClass);
 					active = $(this);
-
-					// on cache les freres
-					hideSiblings(active);
-					// montrer seulement 1 niveau en dessous du niveau actif
-					hideGrandsons(active);
-					// on active l'onglet survolé et tous ses tab parents
 					activeToTop(active);
 				},
 				out : function() {
@@ -62,15 +54,20 @@
 
 		});
 
-		// cache les row petit fils de l'onglet actif de sorte à n'avoir que le niveau n+1
-		function hideGrandsons(active) {
-			$(active.attr(settings.rowTargetAttr)).children(settings.rowElement).hide();
+		// cache toutes les lignes
+		function hideAll() {
+
+			$("#" + settings.tabMenuId + " " + settings.rowElement).each(function() {
+				$(this).hide();
+			});
 		}
 
-		// active l'onglet survolé et tous ses tab parents
+		// active l'onglet survolé ainsi que tous ses tab parents et cache le reste (à optimiser ?)
 		function activeToTop(active) {
-			active.find("a").addClass(settings.activeClass);
-			$(active.attr(settings.rowTargetAttr)).fadeIn();
+			// reset le menu.
+			reset();
+			active.addClass(settings.activeClass);
+			$(active.attr(settings.rowTargetAttr)).show();
 
 			// N'ayez pas peur :D, c'est simple en fait
 			// on sélectionne vers le haut tous les div jusqu'au container du menu (:tabMenu)
@@ -78,47 +75,30 @@
 				// pour chaque div on va remonter au parent puis on va choisir le li dont la valeur vaut l'id du div. Sur le a contenu dans ce li on va
 				// mettre
 				// la classe active
-				$(this).parent().find(settings.activeElement + "[" + settings.rowTargetAttr + "=#" + $(this).attr("id") + "]").find("a").each(function() {
-					// on active le lien
-					$(this).addClass(settings.activeClass);
-				});
 				// on raffiche le div
-				$(this).fadeIn();
-			});
-		}
+				$(this).show();
+				// $(this).children(settings.activeElement).show();
+				$(this).parent().find(settings.activeElement + "[" + settings.rowTargetAttr + "=#" + $(this).attr("id") + "]").addClass(settings.activeClass);
 
-		// masquer tous les fils des siblings (:frères) de l'onglet actif
-		function hideSiblings(active) {
-
-			active.siblings().each(function() {
-				// propre
-				$($(this).attr(settings.rowTargetAttr)).hide();
 			});
 		}
 
 		// permet de restorer le tab initial
 		function restoreTab() {
 
-			// reset les tabs actifs.
-			resetClass(settings.activeClass);
 			// re-set le tab courant.
-			showRootRowOnly();
 			activeToTop(currentTab);
 		}
 
-		// seulement le niveau 1 de visible
-		function showRootRowOnly() {
-			$("#" + settings.rootRowId).children("div").each(function() {
-				$(this).hide();
+		// cache toutes les lignes et désactive tous les tabs
+		function reset() {
+			hideAll();
+			$("#" + settings.tabMenuId + " " + settings.activeElement).each(function() {
+				$(this).removeClass(settings.activeClass);
 			});
 		}
 
-		function resetClass(classToRemove) {
-			$("#" + settings.tabMenuId + " " + settings.activeElement + " a").each(function() {
-				$(this).removeClass(classToRemove);
-			});
-		}
-
+		// prend un menu json et l'injecte dans le tabMenu
 		function buildMenu(jsonMenu) {
 			console.log("buidling jsonMenu");
 		}
